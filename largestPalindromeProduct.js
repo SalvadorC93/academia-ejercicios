52192df0b022a084280f403954fc387bde6b5097
SalvadorC93 <salvador.cuellar93@gmail.com>
2019-01-22 23:21:41 -0600

    Ejercicios JS

--- largestPalindromeProduct.js
+++ largestPalindromeProduct.js
@@ -6,37 +6,41 @@
  *                   the palindromeNumber and the palindromeNumber itself.
  */
 module.exports = function(digits){
-  var factor_0 = 0;
-  var factor_1 = 0;
-  var palindromeNumber = 0;
-  var value;
-  var array = [];
-  // do your work here
+ var factor_0 = 0;
+var factor_1 = 0;
+var palindromeNumber = 0;
+var value;
+// do your work here
 
-if(n==2){
-  for(var x=10;x<100;x++){
-    for(var y = 10;y<100;y++){
-      value = x*y;
-      if (value == parseInt(reverse_a_number(value),10) ){
-        array.push(value);
+function reverse_a_number(n) {
+  n = n + "";
+  return n.split("").reverse().join("");
+}
+
+if (digits === 2) {
+  for (var x = 10; x < 100; x++) {
+    for (var y = 10; y < 100; y++) {
+      value = x * y;
+      if (value == parseInt(reverse_a_number(value), 10)) {
+        factor_0=x;
+        factor_1=y;
       }
     }
   }
-  factor_0 =array[array.length-1];
-}else
+} else
 
-if(n==3){
-  for(var x=100;x<1000;x++){
-    for(var y = 100;y<1000;y++){
-      value = x*y;
-      if (value == parseInt(reverse_a_number(value),10) ){
-        array.push(value);
+  if (digits === 3) {
+    for (var x = 100; x < 1000; x++) {
+      for (var y = 100; y < 1000; y++) {
+        value = x * y;
+        if (value == parseInt(reverse_a_number(value), 10)) {
+          factor_0 = x;
+          factor_1 = y;
+        }
       }
     }
-  }
-factor_1 = array[array.length-1];
-}else
-console.log("error");
+  } else
+    console.log("error");
 
 
   return {
--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,18 +1,18 @@
 exports.largestPrimeFactor = function(n){
-  var integer = 600851475143;
-var    primeArray = [];
-var    isPrime;
-var    higest;
-var    h;
+var integer = 600851475143;
+var primeArray = [];
+var isPrime;
+var highest;
+var h;
 
 //find divisors starting with 2
 
-for(i = 2; i <= integer; i++){
-  if (integer % i==0) {
+for (var i = 2; i <= integer; i++) {
+  if (integer % i === 0) {
 
     //check if divisor is prime
-    for(var j = 2; j <= i/2; j++) {
-      if(i % j == 0) {
+    for (var j = 2; j <= i / 2; j++) {
+      if (i % j === 0) {
         isPrime = false;
       } else {
         isPrime = true;
@@ -21,15 +21,17 @@ for(i = 2; i <= integer; i++){
 
     //if the divisor is prime
 
-    if (isPrime == true) {
+    if (isPrime === true) {
       //divide integer by prime factor & factor store in array primeArray
       integer /= i
       primeArray.push(i);
     }
-  }   
+  }
 }
-rimeArray.length;
-highest = primeArray[h-1];
+h = primeArray.length;
+highest = primeArray[h - 1];
+
+console.log(highest);
 
 return highest;
 };
